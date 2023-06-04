fdd35e423eb368fb17b55cdf6a6117ac51e7c5be
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2023-06-04 12:19:15 +0200

    initial commit

---
+++ task_1.py
@@ -0,0 +1,17 @@
+# This is a simple calculator program.
+
+def add(x, y):
+   return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   return x / y
+
+# Task 1: Implement a function for Exponentiation operation, similar to the existing functions above.
+# Task 2: Create an interactive user input for the calculator, allowing the user to pick an operation and input numbers.
+
