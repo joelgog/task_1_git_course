02f98474158709ef835398f038703732342b0e5a
Partynumbers42 <118724611+Partynumbers42@users.noreply.github.com>
2024-06-04 16:21:16 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -15,8 +15,37 @@ def divide(x, y):
    else:
         return "Cannot divide by zero."
 
+def exponentiate(x,y):
+    return x**y
 
+keep_running = True
 
+def read_code():
+    s = input("What operation do you want to do (+,-,*,/,^):")
+    
+    if s=='exit':
+        global keep_running
+        keep_running = False
+        return 'Closing'
+    
+    x = input("First number:")
+    y = input("Second number:")
+    print('You entered: '+x+s+y)
+    x=float(x)
+    y=float(y)
+    if s=='+':
+        return add(x, y)
+    elif s=='-':
+        return subtract(x, y)
+    elif s=='*':
+        return multiply(x, y)
+    elif s=='/':
+        return divide(x, y)
+    elif s=='^':
+        return exponentiate(x, y)
+    
+while keep_running:
+    print(read_code())
 
 # Task 1: Implement a function for Exponentiation operation, similar to the existing functions above.
 # Task 2: Create an interactive user input for the calculator, allowing the user to pick an operation and input numbers.
