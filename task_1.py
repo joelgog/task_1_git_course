acb6217bcc624e324261ce0684e396421387c1a7
Joel <joel.gogniat@hotmail.com>
2024-06-04 16:22:50 +0200

    new exp function

--- task_1.py
+++ task_1.py
@@ -1,22 +1,42 @@
 # This is a simple calculator program.
 
 def add(x, y):
-   return x + y
+	return x + y
 
 def subtract(x, y):
-   return x - y
+	return x - y
 
 def multiply(x, y):
-   return x * y
+	return x * y
 
 def divide(x, y):
-   if y != 0:  # Check for division by zero
-        return x / y
-   else:
-        return "Cannot divide by zero."
+	if y != 0:  # Check for division by zero
+        	return x / y
+	else:
+        	return "Cannot divide by zero."
+
+def exponentiate(x, y):
+	return x ** y
+
+def read():
+	op = input("Enter operation:")
+	x = float(input("Enter first number:"))
+	y = float(input("Enter second number:"))
 
 
+	if op == "add":
+		return add(x, y)
+	elif op == "subtract":
+		return subtract(x, y)
+	elif op == "multiply":
+		return multiply(x, y)
+	elif op == "divide":
+		return divide(x, y)
+	elif op == "exponentiate":
+		return exponentiate(x, y)
 
+while True:
+	print(read())
 
 # Task 1: Implement a function for Exponentiation operation, similar to the existing functions above.
 # Task 2: Create an interactive user input for the calculator, allowing the user to pick an operation and input numbers.
